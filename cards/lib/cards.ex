eacb4aa738e468dc8ccea933f336867438816de4
lokideos <archmage09@gmail.com>
2019-05-02 20:06:54 +0300

    Add create_hand method

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -31,4 +31,10 @@ defmodule Cards do
       {:error, _reason} -> "That fils does not exist"
     end
   end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+  end
 end
